420fd9ce0dd6bd639632f8afad0322b8a5677068
didyoulikemysunglasses <78660851+didyoulikemysunglasses@users.noreply.github.com>
2021-02-06 15:55:19 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 	fetch('./intro.txt').then(response => {
-		response.text(result => {
+		response.text().then(result => {
 			document.getElementById('textParagraph').innerHTML = result;
 		});
 	});
